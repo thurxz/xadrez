d33c70ff5331d7cfb809ee6fbc4a8178c4265e3e
Arthur <125562467+thurxz@users.noreply.github.com>
2024-12-05 19:16:32 -0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 class Tabuleiro {
     constructor() {
-        this.nome = "Tabuleiro";
-
         this.tabuleiro = [];
         for (let linha = 0; linha < 8; linha++) {
             this.tabuleiro[linha] = [];
@@ -9,25 +7,46 @@ class Tabuleiro {
                 this.tabuleiro[linha][coluna] = new Casa(linha, coluna);
             }
         }
-        console.log(this.tabuleiro);
-
         this.selecionada = null;
+
+        this.criarTabuleiroVisual();
+    }
+
+    criarTabuleiroVisual() {
+        const tabuleiroHtml = document.getElementById('tabuleiro');
+        tabuleiroHtml.innerHTML = '';
+        for (let linha = 0; linha < 8; linha++) {
+            for (let coluna = 0; coluna < 8; coluna++) {
+                const casa = this.tabuleiro[linha][coluna];
+                tabuleiroHtml.appendChild(casa.elementoHtml);
+
+                casa.elementoHtml.addEventListener('click', () => {
+                    this.clicarCasa(casa);
+                });
+            }
+        }
     }
 
     colocarPeca(peca, linha, coluna) {
         const casa = this.tabuleiro[linha][coluna];
-        casa.peca = peca;
-        casa.elementoHtml.innerHTML = peca.simbolo;
+        casa.setPeca(peca);
     }
 
     clicarCasa(casa) {
         if (this.selecionada && this.selecionada.peca) {
             const peca = this.selecionada.peca;
-            casa.setPeca(peca);
-            this.selecionada.setPeca(null);
-            this.selecionada = null;
-        }else if (casa.peca) {
-            this.selecionada = casa;
+            const movimentos = peca.movimentosPossiveis(this.tabuleiro);
+
+            if (movimentos.some(mov => mov.linha === casa.linha && mov.coluna === casa.coluna)) {
+                casa.setPeca(peca);
+                this.selecionada.setPeca(null);
+                peca.moverPara(casa.linha, casa.coluna);
+                this.selecionada = null;
+            } else {
+                this.selecionada = null; 
+            }
+        } else if (casa.peca) {
+            this.selecionada = casa; 
         }
     }
 }
@@ -40,17 +59,11 @@ class Casa {
 
         this.elementoHtml = document.createElement('div');
         this.elementoHtml.classList.add('casa');
-
-        if ((linha + coluna) % 2 == 0) {
+        if ((linha + coluna) % 2 === 0) {
             this.elementoHtml.classList.add('clara');
         } else {
             this.elementoHtml.classList.add('escura');
         }
-        document.getElementById('tabuleiro').appendChild(this.elementoHtml);
-
-        this.elementoHtml.addEventListener('click', () => {
-            tabuleiro.clicarCasa(this);
-        })
     }
 
     setPeca(peca) {
@@ -64,10 +77,10 @@ class Peca {
         this.cor = cor;
         this.linha = linha;
         this.coluna = coluna;
-        this.simbolo = "";
+        this.simbolo = '';
     }
 
-    movimentosPossiveis() {
+    movimentosPossiveis(tabuleiro) {
         return [];
     }
 
@@ -81,13 +94,52 @@ class Peao extends Peca {
     constructor(cor, linha, coluna) {
         super(cor, linha, coluna);
         this.simbolo = cor === 'branca' ? '&#9817;' : '&#9823;';
+        this.movimentoInicial = true; 
+    }
+
+    movimentosPossiveis(tabuleiro) {
+        return this.moverLinhaColuna(tabuleiro);
     }
 }
+
 
 class Torre extends Peca {
     constructor(cor, linha, coluna) {
         super(cor, linha, coluna);
         this.simbolo = cor === 'branca' ? '&#9814;' : '&#9820;';
+    }
+
+    movimentosPossiveis(tabuleiro) {
+        return this.moverLinhaColuna(tabuleiro);
+    }
+
+    moverLinhaColuna(tabuleiro) {
+        const movimentos = [];
+        const direcoes = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+        for (const [dLinha, dColuna] of direcoes) {
+            let novaLinha = this.linha;
+            let novaColuna = this.coluna;
+
+            while (true) {
+                novaLinha += dLinha;
+                novaColuna += dColuna;
+
+                if (novaLinha < 0 || novaLinha >= 8 || novaColuna < 0 || novaColuna >= 8) break;
+
+                const casa = tabuleiro[novaLinha][novaColuna];
+                if (casa.peca) {
+                    if (casa.peca.cor !== this.cor) {
+                        movimentos.push({ linha: novaLinha, coluna: novaColuna });
+                    }
+                    break;
+                }
+
+                movimentos.push({ linha: novaLinha, coluna: novaColuna });
+            }
+        }
+
+        return movimentos;
     }
 }
 
@@ -96,6 +148,28 @@ class Cavalo extends Peca {
         super(cor, linha, coluna);
         this.simbolo = cor === 'branca' ? '&#9816;' : '&#9822;';
     }
+
+    movimentosPossiveis(tabuleiro) {
+        const movimentos = [];
+        const saltos = [
+            [2, 1], [2, -1], [-2, 1], [-2, -1],
+            [1, 2], [1, -2], [-1, 2], [-1, -2],
+        ];
+
+        for (const [dLinha, dColuna] of saltos) {
+            const novaLinha = this.linha + dLinha;
+            const novaColuna = this.coluna + dColuna;
+
+            if (novaLinha >= 0 && novaLinha < 8 && novaColuna >= 0 && novaColuna < 8) {
+                const casa = tabuleiro[novaLinha][novaColuna];
+                if (!casa.peca || casa.peca.cor !== this.cor) {
+                    movimentos.push({ linha: novaLinha, coluna: novaColuna });
+                }
+            }
+        }
+
+        return movimentos;
+    }
 }
 
 class Bispo extends Peca {
@@ -103,12 +177,38 @@ class Bispo extends Peca {
         super(cor, linha, coluna);
         this.simbolo = cor === 'branca' ? '&#9815;' : '&#9821;';
     }
-}
 
-class Rei extends Peca {
-    constructor(cor, linha, coluna) {
-        super(cor, linha, coluna);
-        this.simbolo = cor === 'branca' ? '&#9812;' : '&#9818;';
+    movimentosPossiveis(tabuleiro) {
+        return this.moverDiagonal(tabuleiro);
+    }
+
+    moverDiagonal(tabuleiro) {
+        const movimentos = [];
+        const direcoes = [[1, 1], [-1, -1], [1, -1], [-1, 1]];
+
+        for (const [dLinha, dColuna] of direcoes) {
+            let novaLinha = this.linha;
+            let novaColuna = this.coluna;
+
+            while (true) {
+                novaLinha += dLinha;
+                novaColuna += dColuna;
+
+                if (novaLinha < 0 || novaLinha >= 8 || novaColuna < 0 || novaColuna >= 8) break;
+
+                const casa = tabuleiro[novaLinha][novaColuna];
+                if (casa.peca) {
+                    if (casa.peca.cor !== this.cor) {
+                        movimentos.push({ linha: novaLinha, coluna: novaColuna });
+                    }
+                    break;
+                }
+
+                movimentos.push({ linha: novaLinha, coluna: novaColuna });
+            }
+        }
+
+        return movimentos;
     }
 }
 
@@ -117,25 +217,64 @@ class Rainha extends Peca {
         super(cor, linha, coluna);
         this.simbolo = cor === 'branca' ? '&#9813;' : '&#9819;';
     }
+
+    movimentosPossiveis(tabuleiro) {
+        return [
+            ...this.moverLinhaColuna(tabuleiro),
+            ...this.moverDiagonal(tabuleiro)
+        ];
+    }
+
+    moverLinhaColuna(tabuleiro) {
+        return new Torre(this.cor, this.linha, this.coluna).moverLinhaColuna(tabuleiro);
+    }
+
+    moverDiagonal(tabuleiro) {
+        return new Bispo(this.cor, this.linha, this.coluna).moverDiagonal(tabuleiro);
+    }
+}
+
+class Rei extends Peca {
+    constructor(cor, linha, coluna) {
+        super(cor, linha, coluna);
+        this.simbolo = cor === 'branca' ? '&#9812;' : '&#9818;';
+    }
+
+    movimentosPossiveis(tabuleiro) {
+        const movimentos = [];
+        const direcoes = [
+            [1, 0], [-1, 0], [0, 1], [0, -1],
+            [1, 1], [1, -1], [-1, 1], [-1, -1],
+        ];
+
+        for (const [dLinha, dColuna] of direcoes) {
+            const novaLinha = this.linha + dLinha;
+            const novaColuna = this.coluna + dColuna;
+
+            if (novaLinha >= 0 && novaLinha < 8 && novaColuna >= 0 && novaColuna < 8) {
+                const casa = tabuleiro[novaLinha][novaColuna];
+                if (!casa.peca || casa.peca.cor !== this.cor) {
+                    movimentos.push({ linha: novaLinha, coluna: novaColuna });
+                }
+            }
+        }
+
+        return movimentos;
+    }
 }
 
 const tabuleiro = new Tabuleiro();
 
 const pecasBrancas = [
-    new Peao('branca', 1, 0), new Peao('branca', 1, 1), new Peao('branca', 1, 2), new Peao('branca', 1, 3),
-    new Peao('branca', 1, 4), new Peao('branca', 1, 5), new Peao('branca', 1, 6), new Peao('branca', 1, 7),
-    new Torre('branca', 0, 0), new Torre('branca', 0, 7), new Cavalo('branca', 0, 1), new Cavalo('branca', 0, 6),
-    new Bispo('branca', 0, 2), new Bispo('branca', 0, 5), new Rainha('branca', 0, 3), new Rei('branca', 0, 4)
+    new Torre('branca', 0, 0), new Cavalo('branca', 0, 1), new Bispo('branca', 0, 2), new Rainha('branca', 0, 3),
+    new Rei('branca', 0, 4), new Bispo('branca', 0, 5), new Cavalo('branca', 0, 6), new Torre('branca', 0, 7),
+    ...Array.from({ length: 8 }, (_, col) => new Peao('branca', 1, col))
 ];
 
-peao1 = new Peao('preta', 6, 0);
-tabuleiro.colocarPeca(peao1,6,0);
-
 const pecasPretas = [
-    new Peao('preta', 6, 0), new Peao('preta', 6, 1), new Peao('preta', 6, 2), new Peao('preta', 6, 3),
-    new Peao('preta', 6, 4), new Peao('preta', 6, 5), new Peao('preta', 6, 6), new Peao('preta', 6, 7),
-    new Torre('preta', 7, 0), new Torre('preta', 7, 7), new Cavalo('preta', 7, 1), new Cavalo('preta', 7, 6),
-    new Bispo('preta', 7, 2), new Bispo('preta', 7, 5), new Rainha('preta', 7, 3), new Rei('preta', 7, 4)
+    new Torre('preta', 7, 0), new Cavalo('preta', 7, 1), new Bispo('preta', 7, 2), new Rainha('preta', 7, 3),
+    new Rei('preta', 7, 4), new Bispo('preta', 7, 5), new Cavalo('preta', 7, 6), new Torre('preta', 7, 7),
+    ...Array.from({ length: 8 }, (_, col) => new Peao('preta', 6, col))
 ];
 
 pecasBrancas.forEach(peca => tabuleiro.colocarPeca(peca, peca.linha, peca.coluna));
